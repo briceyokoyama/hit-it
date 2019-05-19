350f9e6f92c47f653c595e7bb7c5230167870a22
briceyokoyama <briceyokoyama@gmail.com>
2019-05-19 11:55:48 -0700

    Begin adding start screen

--- src/index.js
+++ src/index.js
@@ -4,33 +4,8 @@ const Obstacle = require("./obstacle");
 
 document.addEventListener("DOMContentLoaded", () => {
 
-  let start = false;
-
-  const canvasEl = document.getElementsByTagName("canvas")[0];
-  canvasEl.style.display = "block";
-  canvasEl.height = 600;
-  canvasEl.width = 600;
-  let ctx = canvasEl.getContext("2d");
-  let obs = [];
-
-  const pb = new PlayBall(
-    { pos: [30, 30], vel: [0, 0], radius: 5, color: "#55aacc"}
-  );
-
-  const target = new Target(
-    { pos: [300, 300]}
-  );
-
-  const obs1 = new Obstacle(
-    { pos: [400, 300], height: 300, width: 20, angle: 0, color: "#F739A3"}
-  );
-
-  const obs2 = new Obstacle(
-    { pos: [200, 300], height: 300, width: 20, angle: 0, color: "#F739A3"}
-  );
-
-  obs.push(obs1);
-  obs.push(obs2);
+  let start = document.getElementById("start-button");
+  start.addEventListener('click', startGame);
 
   const handleClick = (e) => {
     clicks++;
@@ -40,45 +15,71 @@ document.addEventListener("DOMContentLoaded", () => {
     canvasEl.removeEventListener('click', handleClick);
   }
 
-  canvasEl.addEventListener('click', handleClick)
-  let dist;
-  let score = 0;
-  let clicks = 0;
+  const startGame = () => {
+    const canvasEl = document.getElementsByTagName("canvas")[0];
 
-  const doStuff = () => {
-    ctx.clearRect(0, 0, canvasEl.width, canvasEl.height);
-    ctx.fillStyle = "#000000"
-    ctx.fillRect(0, 0, canvasEl.width, canvasEl.height);
+    canvasEl.style.display = "inline";
+    canvasEl.height = 600;
+    canvasEl.width = 600;
+    let ctx = canvasEl.getContext("2d");
+    let obs = [];
 
-    if (pb.pos[0] + pb.vel[0] > canvasEl.width || pb.pos[0] + pb.vel[0] < 0) {
-      pb.vel[0] = -pb.vel[0];
+    const pb = new PlayBall(
+      { pos: [30, 30], vel: [0, 0], radius: 5, color: "#55aacc"}
+    );
+
+    const target = new Target(
+      { pos: [300, 300]}
+    );
+
+    const obs1 = new Obstacle(
+      { pos: [400, 300], height: 300, width: 20, angle: 0, color: "#F739A3"}
+    );
+
+    const obs2 = new Obstacle(
+      { pos: [200, 300], height: 300, width: 20, angle: 0, color: "#F739A3"}
+    );
+
+    obs.push(obs1);
+    obs.push(obs2);
+
+    canvasEl.addEventListener('click', handleClick)
+    let dist;
+    let score = 0;
+    let clicks = 0;
+
+    const doStuff = () => {
+      ctx.clearRect(0, 0, canvasEl.width, canvasEl.height);
+      ctx.fillStyle = "#000000"
+      ctx.fillRect(0, 0, canvasEl.width, canvasEl.height);
+  
+      if (pb.pos[0] + pb.vel[0] > canvasEl.width || pb.pos[0] + pb.vel[0] < 0) {
+        pb.vel[0] = -pb.vel[0];
+      }
+  
+      if (pb.pos[1] + pb.vel[1] > canvasEl.width || pb.pos[1] + pb.vel[1] < 0) {
+        pb.vel[1] = -pb.vel[1];
+      }
+  
+      obs.forEach(ob => {
+        ob.checkCollisions(pb);
+        ob.draw(ctx);
+      })
+  
+      dist = pb.getDistance(target);
+      target.draw(ctx);
+      pb.draw(ctx);
+      ctx.textAlign = "end";
+      ctx.font = '24px Arial';
+      ctx.fillStyle = "#ffffff";
+      ctx.fillText(`dist: ${dist}`, 585, 25);
+      ctx.fillText(`clicks: ${clicks}`, 585, 60);
+      if (pb.move()) {
+        canvasEl.addEventListener('click', handleClick);
+      };
+      requestAnimationFrame(doStuff);
     }
-
-    if (pb.pos[1] + pb.vel[1] > canvasEl.width || pb.pos[1] + pb.vel[1] < 0) {
-      pb.vel[1] = -pb.vel[1];
-    }
-
-    obs.forEach(ob => {
-      ob.checkCollisions(pb);
-      ob.draw(ctx);
-    })
-
-    dist = pb.getDistance(target);
-    target.draw(ctx);
-    pb.draw(ctx);
-    ctx.textAlign = "end";
-    ctx.font = '24px Arial';
-    ctx.fillStyle = "#ffffff";
-    ctx.fillText(`dist: ${dist}`, 585, 25);
-    ctx.fillText(`clicks: ${clicks}`, 585, 60);
-    if (pb.move()) {
-      canvasEl.addEventListener('click', handleClick);
-    };
+   
     requestAnimationFrame(doStuff);
   }
-
-  const startGame = () => {
-    
-  }
-  requestAnimationFrame(doStuff);
 })

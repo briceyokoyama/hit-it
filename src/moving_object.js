54af4ce1537a36f1b525ff924d01b749fedb4303
briceyokoyama <briceyokoyama@gmail.com>
2019-05-30 23:36:01 -0700

    Modify right and left collision bounds

--- src/moving_object.js
+++ src/moving_object.js
@@ -51,45 +51,87 @@ class MovingObject {
   }
 
   collisionLeft(ob) {
+
     let slope = this.vel[1]/this.vel[0];
-    let wallSlope = (ob.ytopLeftPt - ob.ybottomLeftPt) / (ob.xtopLeftPt - ob.xbottomLeftPt);
-    let intersectionX = ((slope*this.pos[0] - this.pos[1]) - (wallSlope*ob.xbottomLeftPt - ob.ybottomLeftPt))/(slope - wallSlope);
-    let intersectionY = slope*intersectionX - (slope*this.pos[0] - this.pos[1]);
 
-    let yPrevDiff;
-    let yCurrDiff;
-
-    if (this.prevPos[0] < intersectionX && this.pos[0] > intersectionX) {
-      debugger
-      yPrevDiff = this.prevPos[1] - intersectionY;
-      yCurrDiff = this.pos[1] - intersectionY;
-      if(yPrevDiff*yCurrDiff <= 0) {
-        this.pos[0] = intersectionX;
-        this.pos[1] = intersectionY;
-        this.vel[0] = -this.vel[0]
-        return true;
+    if (ob.angle === 0) {
+      let wallDim = ob.pos[0] - ob.width/2;
+      let intersectionPT = this.prevPos[1] + slope * (wallDim - this.prevPos[0]);
+      let yLowerBound = ob.pos[1] - ob.height/2;
+      let yUpperBound = ob.pos[1] + ob.height/2;
+      if (this.prevPos[0] < wallDim) {
+        if (this.pos[0] > wallDim) {
+          if(intersectionPT > yLowerBound && intersectionPT < yUpperBound) {
+            this.pos[0] = wallDim;
+            this.pos[1] = intersectionPT;
+            this.vel[0] = -this.vel[0]
+            return true;
+          }
+        }
       }
-    }
     return false;
-  }
+    } else {
+      let wallSlope = (ob.ytopLeftPt - ob.ybottomLeftPt) / (ob.xtopLeftPt - ob.xbottomLeftPt);
+      let intersectionX = ((slope*this.pos[0] - this.pos[1]) - (wallSlope*ob.xbottomLeftPt - ob.ybottomLeftPt))/(slope - wallSlope);
+      let intersectionY = slope*intersectionX - (slope*this.pos[0] - this.pos[1]);
 
-  collisionRight(obstacle) {
-    let slope = this.vel[1]/this.vel[0];
-    let wallDim = obstacle.pos[0] + obstacle.width/2;
-    let intersectionPT = this.prevPos[1] + slope * (wallDim - this.prevPos[0]);
-    let yLowerBound = obstacle.pos[1] - obstacle.height/2;
-    let yUpperBound = obstacle.pos[1] + obstacle.height/2;
-    if (this.prevPos[0] > wallDim) {
-      if (this.pos[0] < wallDim) {
-        if(intersectionPT > yLowerBound && intersectionPT < yUpperBound) {
-          this.pos[0] = wallDim;
-          this.pos[1] = intersectionPT;
+      let yPrevDiff;
+      let yCurrDiff;
+
+      if (intersectionY > ob.ytopLeftPt && intersectionY < ob.ybottomLeftPt && this.prevPos[0] < intersectionX && this.pos[0] > intersectionX) {
+        yPrevDiff = this.prevPos[1] - intersectionY;
+        yCurrDiff = this.pos[1] - intersectionY;
+        if(yPrevDiff*yCurrDiff <= 0) {
+          this.pos[0] = intersectionX;
+          this.pos[1] = intersectionY;
           this.vel[0] = -this.vel[0]
           return true;
         }
       }
+      return false;
     }
-    return false;
+  }
+
+  collisionRight(ob) {
+
+    let slope = this.vel[1]/this.vel[0];
+
+    if (ob.angle === 0) {
+      let wallDim = ob.pos[0] + ob.width/2;
+      let intersectionPT = this.prevPos[1] + slope * (wallDim - this.prevPos[0]);
+      let yLowerBound = ob.pos[1] - ob.height/2;
+      let yUpperBound = ob.pos[1] + ob.height/2;
+      if (this.prevPos[0] > wallDim) {
+        if (this.pos[0] < wallDim) {
+          if(intersectionPT > yLowerBound && intersectionPT < yUpperBound) {
+            this.pos[0] = wallDim;
+            this.pos[1] = intersectionPT;
+            this.vel[0] = -this.vel[0]
+            return true;
+          }
+        }
+      }
+      return false;
+    } else {
+      let wallSlope = (ob.ytopRightPt - ob.ybottomRightPt) / (ob.xtopRightPt - ob.xbottomRightPt);
+      let intersectionX = ((slope*this.pos[0] - this.pos[1]) - (wallSlope*ob.xbottomRightPt - ob.ybottomRightPt))/(slope - wallSlope);
+      let intersectionY = slope*intersectionX - (slope*this.pos[0] - this.pos[1]);
+
+      let yPrevDiff;
+      let yCurrDiff;
+
+      if (intersectionY > ob.ytopRightPt && intersectionY < ob.ybottomRightPt && this.prevPos[0] > intersectionX && this.pos[0] < intersectionX) {
+        yPrevDiff = this.prevPos[1] - intersectionY;
+        yCurrDiff = this.pos[1] - intersectionY;
+        if(yPrevDiff*yCurrDiff <= 0) {
+          this.pos[0] = intersectionX;
+          this.pos[1] = intersectionY;
+          this.vel[0] = -this.vel[0]
+          return true;
+        }
+      }
+      return false;
+    }
   }
 
   collisionTop(obstacle) {
